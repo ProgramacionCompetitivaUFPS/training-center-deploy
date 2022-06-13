a326e7c6c01e1aa29f6943289b84565f2827f1b4
cryselayala98 <criseljazminal@ufps.edu.co>
2022-06-13 15:42:51 -0500

    Ajustes resoluciones pantallas

--- frontend/dist/vendors~56d7304d.361534173270245b4185.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{q4sD:function(n,w,o){}}]);
-//# sourceMappingURL=vendors~56d7304d.361534173270245b4185.bundle.map
---
+++ frontend/dist/vendors~56d7304d.5e3adf278075c89f3097.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{q4sD:function(n,w,o){}}]);
+//# sourceMappingURL=vendors~56d7304d.5e3adf278075c89f3097.bundle.map

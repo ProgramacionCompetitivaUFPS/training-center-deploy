b0824e1e6479d7d7606cb7bef7693fb7d146c1db
Valentina Rico <paulavalentinarlin@ufps.edu.co>
2023-06-27 14:57:09 -0500

    Actualizacion dist

---
+++ frontend/dist/vendors~56d7304d.badcbc5430b3d373e5a5.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{q4sD:function(n,w,o){}}]);
+//# sourceMappingURL=vendors~56d7304d.badcbc5430b3d373e5a5.bundle.map
--- frontend/dist/vendors~56d7304d.ea93b3fb7fa65d82e2ab.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{q4sD:function(n,w,o){}}]);
-//# sourceMappingURL=vendors~56d7304d.ea93b3fb7fa65d82e2ab.bundle.map
